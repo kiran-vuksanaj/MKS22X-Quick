ce25214528cb203831c338db31bdc2bb5ed4dded
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-13 10:16:14 -0400

    changed while statement, various test/print changes

--- Quick.java
+++ Quick.java
@@ -2,7 +2,22 @@ import java.util.Arrays;
 public class Quick{
   public static void main(String[] args){
     //testing partition
+    /*
+    int[] data = {-100,100,-99,99,-88,100,0,3,2};
+    int piv = partition(1,4,data);
+    System.out.println(piv+" "+Arrays.toString(Arrays.copyOfRange(data,1,4)));
+    */
     int[] sorted = {-100,-99,-88,0,2,3,99,100,100};
+    int[] ary = {-100,100,-99,99,-88,100,0,3,2};
+    int val = quickSelect(ary,4);
+    int success = 0;
+    while(val==2){
+      success ++;
+      //System.out.println("-----");
+      val = quickSelect(ary,4);
+    }
+    System.out.println(Arrays.toString(ary));
+    /*
     for(int i=0;i<9;i++){
       int[] ary = {-100,100,-99,99,-88,100,0,3,2};
       //System.out.println(Arrays.toString(ary));
@@ -10,6 +25,7 @@ public class Quick{
       System.out.println(val);
       if(val != sorted[i]) System.out.println("error: "+Arrays.toString(ary));
     }
+    //*/
   }
   public static int partition(int start,int end,int[] data){
     int pivot = randBetween(start,end);
@@ -42,19 +58,22 @@ public class Quick{
   public static int quickSelect(int[] data,int k){
     int left = 0;
     int right = data.length;
-    while(true){
+    while(right-left >= 2){
       //System.out.println(left + " " + right);
       //for(int i=left;i<right;i++) System.out.print(data[i]+",");
       //System.out.println("");
       int pivot = partition(left,right,data);
-      //System.out.println(pivot);
       if(pivot == k) return data[k];
       if(pivot >  k) right = pivot;
       if(pivot <  k) left = pivot;
-      if(right-left == 2) {
-        System.out.println("boundsy");
-        return data[left+1];
-      }
+      //System.out.println(left+" "+right);
+      //System.out.println(pivot);
+      //System.out.println(Arrays.toString(data));
     }
+    if(right-left < 2){
+      System.out.println("mmm");
+      return data[left];
+    }
+    else return data[left+1];
   }
 }
